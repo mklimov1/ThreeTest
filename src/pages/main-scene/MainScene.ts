efc3d47ad40e4135d7b770a22c4cf07d06353633
Maksim Klimov <maxklimov13@yandex.ru>
2025-11-08 21:38:39 +0100

    add cube

--- src/pages/main-scene/MainScene.ts
+++ src/pages/main-scene/MainScene.ts
@@ -1,13 +1,26 @@
-import { PerspectiveCamera } from 'three';
+import { BoxGeometry, Color, Mesh, MeshBasicMaterial, PerspectiveCamera } from 'three';
 
 import { appEventEmitter, Scene, type Size } from '@/shared';
 
 export class MainScene extends Scene {
   private camera!: PerspectiveCamera;
 
+  private createCube() {
+    const geometry = new BoxGeometry( 1, 1, 1 );
+    const material = new MeshBasicMaterial( { color: 0x00ff00 } );
+    const cube = new Mesh( geometry, material );
+
+    return cube;
+  }
+
   protected create() {
     this.camera = new PerspectiveCamera(75, 1, 0.1, 1000);
-    this.scene.add(this.camera);
+    const cube = this.createCube();
+    this.camera.position.z = 5;
+
+    this.scene.background = new Color('#b8b8b8');
+
+    this.scene.add(this.camera, cube);
   }
 
   load() {}
@@ -19,6 +32,7 @@ export class MainScene extends Scene {
 
   private resize(size: Size) {
     this.camera.aspect = size.width / size.height;
+    this.camera.updateProjectionMatrix();
   }
 
   subscribeEvents() {
